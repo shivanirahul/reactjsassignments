248aa32bc5c538f719a86427290570055b8d2a4b
Shivani Rahul <shivanirahul02006@gmail.com>
2026-06-05 10:15:20 +0530

    Refactor NavigationBar to use a simpler structure

--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -2,44 +2,19 @@ import { Link } from "react-router-dom";
 
 function NavigationBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
-          My Website
-        </Link>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
 
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About Us
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+    </ul>
   );
 }
 
